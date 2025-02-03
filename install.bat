cf8933addfad324220b64d291cf2ef43957f0671
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2025-02-03 18:57:25 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,7 @@
 @echo off
-irm https://get.activated.win | iex
-set-ExecutionPolicy -ExecutionPolicy 'RemoteSigned'
 Install-Script winget-install -Force
 winget-install
-set programs= Google.Chrome Nvidia.GeForceExperience Valve.Steam  qBittorrent.qBittorrent 9NCVDN91XZQP Microsoft.VisualStudioCode OBSProject.OBSStudio Ytmdesktop.Ytmdesktop  Git.Git DBBrowserForSQLite.DBBrowserForSQLite        
+set programs=Waterfox.Waterfox Nvidia.GeForceExperience Vencord.Vesktop Valve.Steam qBittorrent.qBittorrent 9NCVDN91XZQP Microsoft.VisualStudioCode OBSProject.OBSStudio Ytmdesktop.Ytmdesktop  Git.Git DBBrowserForSQLite.DBBrowserForSQLite        
 for %%i in (%programs%) do (
     winget install %%i
 )
