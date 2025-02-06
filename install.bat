980554451c889c3a48f5b1e7a8b8c518faa1f4e4
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2025-02-06 19:11:49 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
 Install-Script winget-install -Force
 winget-install
-set programs=Waterfox.Waterfox StartIsBack.StartAllBack Roblox.Roblox Nvidia.GeForceExperience Vencord.Vesktop Valve.Steam qBittorrent.qBittorrent 9NCVDN91XZQP Microsoft.VisualStudioCode OBSProject.OBSStudio Ytmdesktop.Ytmdesktop  Git.Git DBBrowserForSQLite.DBBrowserForSQLite        
+set programs=Waterfox.Waterfox Docker.DockerDesktop StartIsBack.StartAllBack Roblox.Roblox Nvidia.GeForceExperience Vencord.Vesktop Valve.Steam qBittorrent.qBittorrent 9NCVDN91XZQP Microsoft.VisualStudioCode Syncthing.Syncthing OBSProject.OBSStudio Obsidian.Obsidian Docker.DockerDesktop Ytmdesktop.Ytmdesktop  Git.Git DBBrowserForSQLite.DBBrowserForSQLite        
 for %%i in (%programs%) do (
     winget install %%i
 )
