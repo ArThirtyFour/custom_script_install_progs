faf7c98104e3e12edb96f306d8269bbb339a4b35
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2025-04-03 20:22:24 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -14,7 +14,7 @@ set /p = "Ты Microsoft Store поставил?"
 powershell -Command Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "WingetInstaller.msixbundle"
 powershell -Command Add-AppxPackage -Path "WingetInstaller.msixbundle"
 
-winget install Brave.Brave Vencord.Vesktop qBittorrent.qBittorrent Microsoft.VisualStudioCode Syncthing.Syncthing OBSProject.OBSStudio Obsidian.Obsidian Ytmdesktop.Ytmdesktop Git.Git Postman.Postman Valve.Steam DBBrowserForSQLite.DBBrowserForSQLite StartIsBack.StartAllBack
+winget install Brave.Brave Vencord.Vesktop qBittorrent.qBittorrent Microsoft.VisualStudioCode OBSProject.OBSStudio Ytmdesktop.Ytmdesktop Git.Git Postman.Postman Valve.Steam DBBrowserForSQLite.DBBrowserForSQLite StartIsBack.StartAllBack
 
 
 echo Устанавливаю WSL...
