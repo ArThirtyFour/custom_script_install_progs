6d58e0cca96241525bd9f43d1f42be4b785f68f2
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2025-04-07 22:53:30 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
-
+cls
 echo Устанавливаю драйвер WiFi...
 REM ТУТ EXE МОЕГО ДРАЙВЕРА , ТАК ЧТО ОН ТЕБЕ НЕЗАЧЕМ
 WiFi-23.60.1-Driver64-Win10-Win11.exe
@@ -13,12 +13,12 @@ set /p = "Ты Microsoft Store поставил?"
 
 powershell -Command Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "WingetInstaller.msixbundle"
 powershell -Command Add-AppxPackage -Path "WingetInstaller.msixbundle"
-
-winget install Brave.Brave Vencord.Vesktop qBittorrent.qBittorrent Microsoft.VisualStudioCode OBSProject.OBSStudio Ytmdesktop.Ytmdesktop Git.Git Postman.Postman Valve.Steam DBBrowserForSQLite.DBBrowserForSQLite StartIsBack.StartAllBack
+.\NVIDIA_app_v11.0.2.337.exe   
+winget install 9N0DX20HK701 Brave.Brave Vencord.Vesktop qBittorrent.qBittorrent Microsoft.VisualStudioCode OBSProject.OBSStudio Ytmdesktop.Ytmdesktop Git.Git Postman.Postman Valve.Steam DBBrowserForSQLite.DBBrowserForSQLite RamenSoftware.Windhawk XP89DCGQ3K6VLD XP89DCGQ3K6VLD 9NT1R1C2HH7J 
 
 
 echo Устанавливаю WSL...
 wsl --install -d Ubuntu-22.04
-
+& '.\Docker Desktop Installer.exe'   
 echo Перезагрузка компьютера через 5 секунд...
 shutdown /r /t 5
