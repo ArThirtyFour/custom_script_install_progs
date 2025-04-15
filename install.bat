968f3954c7fecab97e8148c54c843709f74559ab
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2025-04-15 16:12:54 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -14,7 +14,7 @@ set /p = "Ты Microsoft Store поставил?"
 powershell -Command Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "WingetInstaller.msixbundle"
 powershell -Command Add-AppxPackage -Path "WingetInstaller.msixbundle"
 .\NVIDIA_app_v11.0.2.337.exe   
-winget install 9N0DX20HK701 Brave.Brave Vencord.Vesktop qBittorrent.qBittorrent Microsoft.VisualStudioCode OBSProject.OBSStudio Ytmdesktop.Ytmdesktop Git.Git Postman.Postman Valve.Steam DBBrowserForSQLite.DBBrowserForSQLite RamenSoftware.Windhawk XP89DCGQ3K6VLD XP89DCGQ3K6VLD 9NT1R1C2HH7J 
+winget install 9N0DX20HK701 Brave.Brave Vencord.Vesktop qBittorrent.qBittorrent Anysphere.Cursor OBSProject.OBSStudio Ytmdesktop.Ytmdesktop Git.Git Postman.Postman Valve.Steam DBBrowserForSQLite.DBBrowserForSQLite RamenSoftware.Windhawk XP89DCGQ3K6VLD XP89DCGQ3K6VLD 9NT1R1C2HH7J 
 
 
 echo Устанавливаю WSL...
